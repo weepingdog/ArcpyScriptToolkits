48101ef207151db812a854c645433f156c607314
weepingdog <farewellwhu@gmail.com>
2015-10-13 21:24:28 +0800

    Create Delete_FeatureClass_in_GDBs.py
    The GDBs should be with the same structure.
    A1.gdb
    --AD
      --LCA
    A2.gdb
    --AD
      --LCA
    ...

---
+++ Delete_FeatureClass_in_GDBs.py
@@ -0,0 +1,35 @@
+#coding=utf-8
+# -*- coding: UTF-8 -*-
+
+# Description: Delete specified feature class in GDBs 
+#   功能描述：删除多个文件地理数据库内指定的要素类
+
+# Date: 2015/10/13
+
+# Author:Zhang Huaixiang
+
+import os
+import arcpy
+
+# Set input direction
+# 设置文件地理数据库所在的路径
+indir = 'D:\py\inpath'
+
+# Set Data Set
+# 设置数据集名称
+ds = 'AD'
+
+# Set Feature Class
+# 设置要素类名称
+fc = 'LCA'
+
+
+# Get the list of GDBs
+# 获得文件地理数据列表
+GDBs=os.listdir(indir)
+
+for thegdb in GDBs:
+    fc_del = indir + os.sep + thegdb + os.sep+ ds + os.sep + fc 
+    print 'deleting ' + fc_del
+    arcpy.Delete_management(fc_del) 
+print 'done'
